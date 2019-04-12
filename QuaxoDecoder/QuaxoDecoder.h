1622051e49005d6d78169d5f727fce9c635f887b
Javid Sheikh <javid@ingenie.com>
2019-04-12 19:27:01 +0100

    Initial Commit

---
+++ QuaxoDecoder/QuaxoDecoder.h
@@ -0,0 +1,19 @@
+//
+//  QuaxoDecoder.h
+//  QuaxoDecoder
+//
+//  Created by Javid Sheikh on 12/04/2019.
+//  Copyright © 2019 QuaxoDigital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuaxoDecoder.
+FOUNDATION_EXPORT double QuaxoDecoderVersionNumber;
+
+//! Project version string for QuaxoDecoder.
+FOUNDATION_EXPORT const unsigned char QuaxoDecoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuaxoDecoder/PublicHeader.h>
+
+
